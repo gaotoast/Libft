1b8e6b995bd9fbfd85b1990e9b7116142cdc1536
stakada <stakada@student.42tokyo.jp>
2024-10-24 20:48:56 +0900

    Done ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -5,23 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/16 23:23:35 by stakada           #+#    #+#             */
-/*   Updated: 2024/05/03 09:27:01 by stakada          ###   ########.fr       */
+/*   Created: 2024/10/24 20:33:48 by stakada           #+#    #+#             */
+/*   Updated: 2024/10/24 20:36:36 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+void ft_bzero(void *s, size_t n)
 {
+	unsigned char *s_copy;
+
+	s_copy = (unsigned char *)s;
 	while (n--)
-		*(unsigned char *)s++ = '\0';
+		*s_copy++ = '\0';
 }
-
-// #include <stdio.h>
-
-// int main(void)
-// {
-// 	char str[] = "ABC";
-// 	ft_bzero(str, 2);
-// }
