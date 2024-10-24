c7197a07d7e60cd7ed4d78ba86ba2a32881b94e7
stakada <stakada@student.42tokyo.jp>
2024-10-24 20:48:45 +0900

    Done ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -5,28 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/16 23:16:02 by stakada           #+#    #+#             */
-/*   Updated: 2024/05/03 17:55:52 by stakada          ###   ########.fr       */
+/*   Created: 2024/10/24 18:56:35 by stakada           #+#    #+#             */
+/*   Updated: 2024/10/24 20:36:19 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void *ft_memset(void *s, int c, size_t n)
 {
-	void	*start;
-
-	start = b;
-	while (len--)
-		*(unsigned char *)b++ = (unsigned char)c;
-	return (start);
+	unsigned char *s_copy;
+	
+	s_copy = (unsigned char *)s;
+	while (n--)
+		*s_copy++ = (unsigned char)c;
+	return (s);
 }
-
-// #include <stdio.h>
-
-// int main(void)
-// {
-// 	char str[] = "ABCDE";
-// 	printf("%s\n", ft_memset((void *)str, 'A', 3));
-// 	return (0);
-// }
