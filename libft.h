a77312053bc3a45f9339180a068710b1826e3c4a
stakada <stakada@student.42tokyo.jp>
2024-11-08 20:23:02 +0900

    Done ft_calloc, ft_strdup

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 18:48:37 by stakada           #+#    #+#             */
-/*   Updated: 2024/11/08 17:29:36 by stakada          ###   ########.fr       */
+/*   Updated: 2024/11/08 20:21:40 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 # define LIBFT_H
 
 # include <stddef.h>
-
-#include <stdio.h>
+# include <stdint.h>
+# include <limits.h>
+#include <stdlib.h>
 
 int ft_isalpha(int c);
 int ft_isdigit(int c);
@@ -37,5 +38,8 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 char *ft_strnstr(const char *big, const char *little, size_t len);
+int ft_atoi(const char *nptr);
+void *ft_calloc(size_t nmemb, size_t size);
+char *ft_strdup(const char *s);
 
 #endif
