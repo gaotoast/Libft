c432d17ee057ea91aa7cc82ab1198c10896df3dc
stakada <stakada@student.42tokyo.jp>
2024-11-08 18:02:45 +0900

    Done ft_strnstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 18:48:37 by stakada           #+#    #+#             */
-/*   Updated: 2024/11/08 17:24:30 by stakada          ###   ########.fr       */
+/*   Updated: 2024/11/08 17:29:36 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ char *ft_strrchr(const char *s, int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
+char *ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
