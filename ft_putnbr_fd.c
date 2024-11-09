a13d4059b293c466f983bbe68e79ea3dec8547cc
stakada <160503699+gao0325ts@users.noreply.github.com>
2024-11-10 00:15:01 +0900

    Fix ft_strrchr not finding null terminator

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,35 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/29 19:51:55 by stakada           #+#    #+#             */
-/*   Updated: 2024/05/02 05:12:01 by stakada          ###   ########.fr       */
+/*   Created: 2024/11/10 00:02:08 by stakada           #+#    #+#             */
+/*   Updated: 2024/11/10 00:10:04 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-	{
-		write(fd, "-2147483648", 11);
-		return ;
-	}
-	if (n < 0)
-	{
-		write(fd, "-", 1);
-		n = -n;
-	}
-	if (n >= 10)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd(n % 10 + '0', fd);
+    char c;
+    long num;
+
+    num = (long)n;
+    if (num < 0)
+    {
+        write(fd, "-", 1);
+        num = -num;
+    }
+    if (num < 10)
+    {
+        c = num + '0';
+        write(fd, &c, 1);
+        return ;
+    }
+    ft_putnbr_fd(num / 10, fd);
+    c = num % 10 + '0';
+    write(fd, &c, 1);
 }
-
-// #include <stdio.h>
-
-// int main(void)
-// {
-//   ft_putnbr_fd(-2147483648, 1);
-//   printf("%c", '\n');
-//   return (0);
-// }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 19:34:28 by stakada           #+#    #+#             */
-/*   Updated: 2024/11/07 21:41:15 by stakada          ###   ########.fr       */
+/*   Updated: 2024/11/10 00:12:57 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char *ft_strrchr(const char *s, int c)
 {
 	int i;
 
-	i = ft_strlen(s) - 1;
+	i = ft_strlen(s);
 	if ((char)c == '\0')
 		return ((char *)&s[i]);
 	while (i >= 0)
